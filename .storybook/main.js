414919873f20b74f01a8bc23f462cc8420623d01
Herve Desrosiers <hdesrosiers@stingray.com>
2021-12-07 09:33:54 -0500

    stories configuration

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,5 +1,6 @@
 module.exports = {
-  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  // stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  stories: ['../src/components/**/*.stories.js'],
   staticDir: ["../public"],
   addons: [
     "@storybook/addon-links",
