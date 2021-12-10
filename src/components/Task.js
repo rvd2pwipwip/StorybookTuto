de9bece7bc1f3bc8dd6df8fa724f84f603af7ee8
Herve Desrosiers <hdesrosiers@stingray.com>
2021-12-10 13:31:27 -0500

    changed component definitions from function to () => {}

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,26 +1,6 @@
 import PropTypes from "prop-types";
 
-Task.propTypes = {
-  /** Composition of the task */
-  task: PropTypes.shape({
-    /** Id of the task */
-    id: PropTypes.string.isRequired,
-    /** Title of the task */
-    title: PropTypes.string.isRequired,
-    /** Current state of the task */
-    state: PropTypes.string.isRequired,
-  }),
-  /** Event to change the task to archived */
-  onArchiveTask: PropTypes.func,
-  /** Event to change the task to pinned */
-  onPinTask: PropTypes.func,
-};
-
-export default function Task({
-  task: { id, title, state },
-  onArchiveTask,
-  onPinTask,
-}) {
+const Task = ({ task: { id, title, state }, onArchiveTask, onPinTask }) => {
   return (
     <div className={`list-item ${state}`}>
       <label className="checkbox">
@@ -60,4 +40,22 @@ export default function Task({
       </div>
     </div>
   );
-}
+};
+
+Task.propTypes = {
+  /** Composition of the task */
+  task: PropTypes.shape({
+    /** Id of the task */
+    id: PropTypes.string.isRequired,
+    /** Title of the task */
+    title: PropTypes.string.isRequired,
+    /** Current state of the task */
+    state: PropTypes.string.isRequired,
+  }),
+  /** Event to change the task to archived */
+  onArchiveTask: PropTypes.func,
+  /** Event to change the task to pinned */
+  onPinTask: PropTypes.func,
+};
+
+export default Task;
